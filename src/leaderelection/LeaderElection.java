7eab065a0fd1e9e65820e30614d2e67c550a9285
Afonso <alunos@I00CN1110>
2016-11-23 11:58:28 +0000

    O meu segundo commit

--- src/leaderelection/LeaderElection.java
+++ src/leaderelection/LeaderElection.java
@@ -18,6 +18,9 @@ public class LeaderElection {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      
+       
+        
         // TODO code application logic here
     }
     
--- src/leaderelection/Node.java
+++ src/leaderelection/Node.java
@@ -5,7 +5,13 @@
  */
 package leaderelection;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,33 +22,92 @@ public class Node {
     private final int value;
     private boolean delta;
     private Node parent;
-    boolean ack_sent;
+    boolean ackSent;
     int lidid;
+    
     ArrayList<Node> N;
     ArrayList<Node> S;
+    ArrayList<Node> ackValues;
     
+    static boolean input;
+    static int state = 1;
     
+    InetAddress group;
+    String inetAddress = "228.5.6.7";
+    int port = 6789;
     
+
     
     public Node(int id, int value){
         this.id = id;
         this.value = value;
         this.delta = false;
         this.parent = null;
-        this.ack_sent = false;
-        this.lidid = 0;
+        this.ackSent = false;
         this.N = null;
         this.S = null;
+        
+        this.stateMachine();
     }
     
-    private void addNeighbor(Node Ni){
-     
+    public void joinGroup() {
+        
+        try {
+            group = InetAddress.getByName(inetAddress);
+            MulticastSocket s = new MulticastSocket(port);
+            DatagramSocket socket = new DatagramSocket();
+            
+            s.joinGroup(group);
+            
+        } catch (Exception ex) {
+            //TODO : cenas
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void addNeighbor(Node Ni){  //TODO:  
         N.add(Ni);
-        (Ni.N).add();
+        S.add(Ni);
+    }
+    private 
+    
+    private boolean changeInput(){
+        //TODO : 
+        new Thread(){
+            @Override
+            public void run(){
+                
+            }
+        }
+        return input;
+    }
+    
+    private final void stateMachine(){
+            
+
         
-        
+        new Thread(){
+   
+            @Override
+            public void run(){
+                while(true) {
+                    switch (state){
+                        case 1: //espera por input ou election
+                }
+                    
+                    
+                }
+                    
+                    
+                    //state machine were if input from PC or received election
+                    
+                    
+            }
+        }.start();
+    
     
     }
-           
-    
 }
+    
+    
+
