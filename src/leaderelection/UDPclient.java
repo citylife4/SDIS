beff85b61fda83d232d51a5f84d2c012d7275143
Administrator <Administrator@I02CP2106>
2016-11-26 18:04:30 +0000

    Update on UDPclient

--- src/leaderelection/UDPclient.java
+++ src/leaderelection/UDPclient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package leaderelection;
+package outro;
 
 import java.io.IOException;
 import static java.lang.Math.log;
@@ -17,22 +17,39 @@ import java.util.logging.Logger;
  *
  * @author ee12038
  */
-public class UDPclient implements Runnable{
+public final class UDPclient implements Runnable{
 
     private int mcPort;
     private String mcIPStr;
-    private int pid;
+    public int pid;
     private MulticastSocket mcSocket = null;
     private InetAddress mcIPAddress = null;
-    private boolean isRunning;
+    public boolean isRunning;
     byte[] buffer = new byte[2048];
-
+    
+    //DEBUG:
+    private final String log;
     
     public UDPclient(int pid, int port, String IPStr) {
+        super();
         this.pid = pid;
         this.mcIPStr = IPStr;
         this.mcPort = port;
-        
+        log  = "[UDP, Thread: " + Thread.currentThread().getId() + "] ";
+        startConnection();
+    }
+    
+    public void startConnection(){
+        try {
+            System.out.println(log + "Starting Connection ");
+            mcIPAddress = InetAddress.getByName(mcIPStr);
+            mcSocket = new MulticastSocket(mcPort);
+            mcSocket.joinGroup(mcIPAddress);
+            isRunning = true;
+        } catch (IOException ex) {
+            System.err.println(log + "Problem on creating socket");
+            Logger.getLogger(UDPclient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void closeConnection(){
@@ -41,59 +58,67 @@ public class UDPclient implements Runnable{
             mcSocket.close();
         }
         else
-            System.err.println("Is not Running");
+            System.err.println(log + "Is not Running");
     }
     
     @Override
     public void run() {
-        
-        try {
-            mcIPAddress = InetAddress.getByName(mcIPStr);
-            mcSocket = new MulticastSocket(mcPort);
-            mcSocket.joinGroup(mcIPAddress);
-            
-            DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
-            isRunning = true;
-            
-            while(isRunning){
-                try {
-                    
-                    //Wait to receive message
-                    System.out.println( "A Receber ...." );
-                    mcSocket.receive( receivePacket );
-                    String msg = new String(buffer, 0, receivePacket.getLength());
-                    //Node.handlePacket( msg );
-                    System.err.println( "Recebido: " + msg );
-                    receivePacket.setLength(buffer.length);
-                    
-                } catch ( Exception e ) {
-                    System.out.println( e.getMessage() );
-                }
+
+        DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+
+        if(!isRunning){
+            startConnection();
+        }
+        while(isRunning){
+            try {
+                
+                
+                //Wait to receive message
+                //this.sendMessage(1, "2", 3);
+                System.out.println(log +  "A Receber .... " );
+                mcSocket.receive( receivePacket );
+                String msg = new String(buffer, 0, receivePacket.getLength());
+                Node.handlePacket( msg );
+                System.err.println(log +  "Recebido: " + msg );
+                receivePacket.setLength(buffer.length);
+
+            } catch ( Exception e ) {
+                System.err.println(log + "Problem on receive");
+                System.out.println( e.getMessage() );
             }
+        }
                 
             //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        } catch (IOException ex) {
-            Logger.getLogger(UDPclient.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
     }
     
     public void sendMessage(int id, String message, int destination, int mostValued){
         
-        if(isRunning) {
-            String messageFormated = id + "@" + message + "@" + destination + "@" + mostValued;
-            byte[] toSend = new byte[2048];
-            toSend = messageFormated.getBytes();
-        
-            DatagramPacket sendPacket =
-                  new DatagramPacket(toSend, toSend.length);
-            try {
-                mcSocket.send(sendPacket);
-            } catch (IOException ex) {
-                Logger.getLogger(UDPclient.class.getName()).log(Level.SEVERE, null, ex);
+        new Thread() {
+            
+            @Override
+                public void run() {
+                  if(!isRunning) {
+                    startConnection();
+                }
+
+                String messageFormated = id + "@" + message + "@" + destination + "@" + mostValued;
+                byte[] toSend = new byte[2048];
+                toSend = messageFormated.getBytes();
+                System.out.println(log + "Sending: " + messageFormated + " _ "+  Thread.currentThread().getId());
+                
+                DatagramPacket sendPacket =
+                      new DatagramPacket(toSend, toSend.length);
+                sendPacket.setAddress(mcIPAddress);
+                sendPacket.setPort(mcPort);
+                try {
+                    mcSocket.send(sendPacket);
+                } catch (IOException ex) {
+                    System.err.println(log + "Problem Sending");
+                    Logger.getLogger(UDPclient.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-        }
-        else
-            System.err.println("Connection not Started");
-    }
-    
+        }.start();     
+        
+    } 
 }
